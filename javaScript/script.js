9fcbc5620d533c334ba5b32922385e9e308b736b
erick poleto <erick-poleto@hotmail.com>
2020-03-10 09:38:31 -0300

    personal atualizado

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,10 +1,36 @@
+var frase = document.querySelector(".typewriter");
+
+
 
 $(function(){
-    verificaEmail();
-   
+    typeWrite(frase);
+    nav();
 });
 
-function verificaEmail(){
+
+function typeWrite(frase){
+        const texto = frase.innerHTML.split('');
+        frase.innerHTML = '';
+        texto.forEach(function(letra, i){
+            setTimeout(function(){
+                frase.innerHTML += letra;
+            }, 50 * i)});
+    /*setTimeout(function(){
+        
+    })*/
+}
+
+function nav(){
+    $('nav a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+                targetOffset = $(id).offset().top;	
+        $('html, body').animate({ 
+            scrollTop: targetOffset - 100
+        }, 500);
+    });
+}
+/*function verificaEmail(){
     var email = $(".email");
     var enviar = $(".enviar");
     if(email.val() == 0){
@@ -21,4 +47,4 @@ function verificaEmail(){
     
     });
     
-}
+}*/
